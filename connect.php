0177bc0f70862b08aff4f43527a44c1057af0ea0
Olivier <oenvoyage@gmail.com>
2010-11-05 18:12:06 +0100

    blabla comit before pull

--- connect.php
+++ connect.php
@@ -1,8 +1,8 @@
 <?php 
-#$my_server="localhost";
-$my_server="192.168.30.31";
-$my_user="brender";
-$my_password="brender";
+$my_server="localhost";
+#$my_server="192.168.30.31";
+$my_user="root";
+$my_password="video34";
 print "connect.php info :: ---- $my_server,$my_user,$my_password ----";
 $link=mysql_connect($my_server,$my_user,$my_password) or die("unable to connect to mysql server");
 @mysql_select_db("brender");
