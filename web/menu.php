5b99316b725bd76298dae809cfcb515ab8647383
fsiddi <contact@fsiddi.com>
2010-11-06 10:57:10 +0100

    Update menu after stash.

--- web/menu.php
+++ web/menu.php
@@ -3,11 +3,7 @@
 	<ul>	
 		<li> <a class="button grey" href="index.php" >overview</a></li>
 		<li><a class="button grey" href="clients.php" >clients</a></li>
-		<li>
-			<a class="button grey" href="jobs.php" >jobs</a>
-			<a href="jobs.php?no_visual=1" target="main">*</a>
-			<a href="jobs.php?all_projects=1" target="main">+</a>
-		</li>
+		<li><a class="button grey" href="jobs.php" >jobs</a></li>
 		<li><a class="button grey" href="orders.php" >orders</a></li>
 		<li><a class="button grey" href="upload.php" >new job</a></li>
 	</ul>
