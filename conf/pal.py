238f309ce980e647049b9583dd6d2632b83b8488
oenvoyage <oenvoyage@gmail.com>
2010-11-08 11:28:17 +0100

    commit of weekend work on brender

--- conf/pal.py
+++ conf/pal.py
@@ -4,8 +4,9 @@ r=bpy.context.scene.render
 
 r.resolution_x=720
 r.resolution_x=576
-r.pixel_aspect_x=1
+r.pixel_aspect_x=1.79
 r.pixel_aspect_y=1
-r.use_simplify=True
-r.simplify_subdivision=0
+r.use_simplify=False
+r.resolution_percentage=100
+#r.simplify_subdivision=0
 
