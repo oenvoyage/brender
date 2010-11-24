47898a39b4be4b2d5948682f6983094fcb9606db
oenvoyage <oenvoyage@gmail.com>
2010-11-24 20:03:30 +0100

    deleted old web.bak folder

--- web.bak/index.php
+++
@@ -1,4 +0,0 @@
-<frameset cols="120,*" frameborder="no" border="0" framespacing="0">
-  <frame src="menu.php" name="menu" id="menu" />
-  <frame src="overview.php?overview=1" name="main" id="main" />
-</frameset>
