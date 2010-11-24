99a8a9aabd859e41ae2345d984e0f3c02b1747a1
oenvoyage <oenvoyage@gmail.com>
2010-11-24 21:40:31 +0100

    project management : simplified form + corrected list

--- web/projects.php
+++ web/projects.php
@@ -41,8 +41,8 @@ if (!$order_by=$_GET[order_by]) {
 		<td></td>
 		<td> &nbsp; </td>
 		<td> &nbsp; <a href=\"index.php?view=projects&order_by=name\">project</a></td>
-		<td> &nbsp; mac path</td>
-		<td> &nbsp; windows path</td>
+		<td> &nbsp; .blend path</td>
+		<td> &nbsp; output path</td>
 		<td> &nbsp; <a href=\"index.php?view=projects&order_by=rem\">rem</a></td>
 		<td> status </td>
 		<td> &nbsp; </td>
@@ -51,8 +51,12 @@ if (!$order_by=$_GET[order_by]) {
 		$id=$row->id;
 		$name=$row->name;
 		$rem=$row->rem;
-		$mac_path=$row->mac_path;
-		$win_path=$row->win_path;
+		$blend_mac=$row->blend_mac;
+		$blend_win=$row->blend_win;
+		$blend_linux=$row->blend_linux;
+		$output_mac=$row->output_mac;
+		$output_win=$row->output_win;
+		$output_linux=$row->output_linux;
 		$status=$row->status;
 		if ($status=="active") {
 			$status_link='<a href="index.php?view=projects&deactivate=' . $id.'">active</a>';
@@ -69,8 +73,8 @@ if (!$order_by=$_GET[order_by]) {
 			<td bgcolor=$bgcolor>$id</td> 
 			<td bgcolor=$bgcolor>$def</td> 
 			<td bgcolor=$bgcolor><a href=\"index.php?view=projects&def=$id\">$name</a></td> 
-			<td bgcolor=$bgcolor>$mac_path</td> 
-			<td bgcolor=$bgcolor>$win_path</td> 
+			<td bgcolor=$bgcolor>mac: $blend_mac <br/>win: $blend_win<br/>linux: $blend_linux</td> 
+			<td bgcolor=$bgcolor>mac: $output_mac <br/>win: $output_win <br/>linux: $output_linux</td> 
 			<td bgcolor=$bgcolor>$rem</td> 
 			<td bgcolor=$bgcolor>$status_link</td> 
 			<td><a href=\"index.php?view=projects&del=$id\">x</a></td>
@@ -89,35 +93,41 @@ if (!$order_by=$_GET[order_by]) {
 <form action="index.php" method="get">
 		<tr>
 			<td>project title</td>
-			<td><input type="text" name="new_project" value="new project" size=8></td>
-		</tr>
-		<tr>
-			<td>project root mac</td>
-			<td><input type="text" name="blend_mac" value="/blend" size=15></td>
-		</tr>
-		<tr>
-			<td>linux</td>
-			<td><input type="text" name="blend_linux" value="/blend" size=15></td>
-		</tr>
-		<tr>
-			<td>windows</td>		
-			<td><input type="text" name="blend_windows" value="\\blend" size=15></td>	
-		</tr>
-		<tr>
-			<td>output path mac</td>
-			<td><input type="text" name="output_mac" value="/render" size=15></td>
-		</tr>
-		<tr>
-			<td>linux</td>
-			<td><input type="text" name="output_linux" value="/render" size=15></td>
-		</tr>
-		<tr>
-			<td>windows</td>		
-			<td><input type="text" name="output_windows" value="\\render" size=15></td>	
+			<td><input type="text" name="new_project" value="new project" size=18></td>
 		</tr>
 		<tr>
 			<td>remarks</td>
-			<td><input type="text" name="rem" size=18></td>
+			<td><input type="text" name="rem" size=38></td>
+		</tr>
+		<tr>
+			<td colspan=2><b>.blend files path</b></td>
+		</tr>
+		<tr>
+			<td>blend_mac</td>
+			<td><input type="text" name="blend_mac" value="blend/" size=15></td>
+		</tr>
+		<tr>
+			<td>blend_linux</td>
+			<td><input type="text" name="blend_linux" value="blend/" size=15></td>
+		</tr>
+		<tr>
+			<td>blend_windows</td>		
+			<td><input type="text" name="blend_windows" value="\\blend" size=15></td>	
+		</tr>
+		<tr>
+			<td colspan=2><b>output path</b></td>
+		</tr>
+		<tr>
+			<td>output_mac</td>
+			<td><input type="text" name="output_mac" value="render" size=15></td>
+		</tr>
+		<tr>
+			<td>output_linux</td>
+			<td><input type="text" name="output_linux" value="render" size=15></td>
+		</tr>
+		<tr>
+			<td>output_windows</td>		
+			<td><input type="text" name="output_windows" value="\\render" size=15></td>	
 		</tr>
 		<tr>
 			<td><input type="hidden" value="projects" name="view"></td>
