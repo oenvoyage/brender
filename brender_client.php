bfedb147ec2ecbca464c4b47003e6071f163868d
oenvoyage <oenvoyage@gmail.com>
2011-01-13 12:38:00 +0100

    licensing brender as GPL v2 (same as blender)

--- brender_client.php
+++ brender_client.php
@@ -1,5 +1,27 @@
 #!/usr/bin/php -q
 <?php 
+/**
+* ***** BEGIN GPL LICENSE BLOCK *****
+*
+* This file is part of Brender.
+*
+* Brender is free software: you can redistribute it and/or 
+* modify it under the terms of the GNU General Public License 
+* as published by the Free Software Foundation, either version 2 
+* of the License, or any later version.
+* 
+* Brender is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License 
+* along with brender.  If not, see <http://www.gnu.org/licenses/>.
+*
+* ***** BEGIN GPL LICENSE BLOCK *****
+*
+*/
+
 require "functions.php";
 require_once "connect.php";
 print "---- brender client 0.2 ----\n";
