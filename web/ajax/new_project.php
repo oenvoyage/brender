0c9a97664557ac79bbd1be9d80cd46e20e8f1fef
oenvoyage <oenvoyage@gmail.com>
2011-12-29 11:36:27 +0100

    fix : can not create new project another round of win->windows missed out fix forgot the ones in the ajax files

--- web/ajax/new_project.php
+++ web/ajax/new_project.php
@@ -15,16 +15,16 @@ if (isset($project_name)) {
 		$rem = $_POST['rem'];
 		$blend_mac = $_POST['blend_mac'];
 		$blend_linux = $_POST['blend_linux'];
-		$blend_win = $_POST['blend_win'];
+		$blend_windows = $_POST['blend_windows'];
 		$output_mac = $_POST['output_mac'];
 		$output_linux = $_POST['output_linux'];
-		$output_win = $_POST['output_win'];
+		$output_windows = $_POST['output_windows'];
 		if (check_project_exists($project)) {
 			# ooooups project already exists
 			echo "{\"status\":false, \"msg\":\"Epic Fail: project $project already exists, please choose a different name.\"}";
 		}
 		else {
-			$query="INSERT INTO projects VALUES ('','$project','$blend_mac','$blend_linux','$blend_win','$output_mac','$output_win','$output_linux','$rem','active','');";
+			$query="INSERT INTO projects VALUES ('','$project','$blend_mac','$blend_linux','$blend_windows','$output_mac','$output_windows','$output_linux','$rem','active','');";
 			mysql_query($query) or die ($dberror = mysql_error());
 			echo "{\"status\":true, \"msg\":\"new project $project created\"}";
 		}
